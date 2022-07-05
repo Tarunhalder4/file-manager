a0acfea30985c7567b96f74f93c789b6a151758f
TarunHalder4 <tarunhalder9@gmail.com>
2022-07-05 13:13:56 +0530

    make splash activity and app icon

--- app/src/main/java/com/example/filemanagers/Constant.java
+++ app/src/main/java/com/example/filemanagers/Constant.java
@@ -13,36 +13,37 @@ import androidx.core.content.ContextCompat;
 public class Constant {
     public static String PATH ="path";
 
-    public static int PIE_CHAT=1;
-    public static int CONTAINS_INTERNAL_STORAGE=2;
-    public static int INTERNAL_STORAGE=3;
-    public static int BOOKMARK=4;
+    public static final int PIE_CHAT=1;
+    public static final int CONTAINS_INTERNAL_STORAGE=2;
+    public static final int INTERNAL_STORAGE=3;
+    public static final int BOOKMARK=4;
 
-    public static String PHOTO_FOLDER = "Photo";
-    public static String DOWNLOAD_FOLDER = "Download";
-    public static String SAFE_BOX_FOLDER = "SafeBox";
-    public static String MUSIC_FOLDER = "Music";
-    public static String RECENT_FILE = "RecentFile";
-    public static String DOCUMENTS_FOLDER = "Documents";
-    public static String APP_MANAGER_FOLDER = "AppManager";
-    public static String VIDEO_FOLDER = "Video";
-    public static String AAD_TO_QUICK_ACCESS = "Add to quick access";
+    public static final int SPLASH_SCREEN_TIME_OUT=2000;
 
-    public static String DCIM_FOLDER = "DCIM";
-    public static String MOVIES_FOLDER = "Video";
-    public static String PICTURES_FOLDER = "Picture";
+    public static final String PHOTO_FOLDER = "Photo";
+    public static final String DOWNLOAD_FOLDER = "Download";
+    public static final String SAFE_BOX_FOLDER = "SafeBox";
+    public static final String MUSIC_FOLDER = "Music";
+    public static final String RECENT_FILE = "RecentFile";
+    public static final String DOCUMENTS_FOLDER = "Documents";
+    public static final String APP_MANAGER_FOLDER = "AppManager";
+    public static final String VIDEO_FOLDER = "Video";
+    public static final String AAD_TO_QUICK_ACCESS = "Add to quick access";
 
-    public static String INTERNAL_STORAGE_PATH="internal_storage_path";
-    public static String FOLDER_PATH="folder_path";
+    public static final String DCIM_FOLDER = "DCIM";
+    public static final String MOVIES_FOLDER = "Video";
+    public static final String PICTURES_FOLDER = "Picture";
 
-    public static String PHOTO_FILE ="photo";
-    public static String PDF_FILE ="pdf_file";
-    public static String AUDIO_FILE ="audio";
-    public static String VIDEO_FILE = "video";
-    public static String DOCUMENTS_FILE = "documents_file";
-    public static String ALL_FILE = "all_file";
-    public static String All_FILE_FOLDER = "file_folder";
-    public static String INTERNAL_STORAGE_FILE_FOLDER = "internal_storage_file_folder";
+    public static final String INTERNAL_STORAGE_PATH="internal_storage_path";
+    public static final String FOLDER_PATH="folder_path";
+
+    public static final String PHOTO_FILE ="photo";
+    public static final String PDF_FILE ="pdf_file";
+    public static final String AUDIO_FILE ="audio";
+    public static final String VIDEO_FILE = "video";
+    public static final String DOCUMENTS_FILE = "documents_file";
+    public static final String ALL_FILE = "all_file";
+    public static final String INTERNAL_STORAGE_FILE_FOLDER = "internal_storage_file_folder";
 
     public static void requestPermission(Activity activity){
         if(ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)){
